cc5cc97013a7ee463e540ab9b2f9a8dd3eeab880
Stephen Tillman <askmrtillman@gmail.com>
2021-10-23 01:44:07 -0400

    serve html home page

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ module.exports = async function (fastify, opts) {
   fastify.addHook('onRequest', async (request, reply) => {
     const is_post_method = /post/ig.test(request.method);
     const email_address = request.query && request.query.email_address;
-    if(!email_address) reply.send([]);
-    if(is_post_method){      
+    if(is_post_method){
+      if(!email_address) reply.send([]);
       const is_email_valid = isEmail.validate(email_address);
       request.email_address = (is_email_valid ? email_address : "");
     }
+  })
+
+  fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'public'),
   })
 
   // Do not touch the following lines
