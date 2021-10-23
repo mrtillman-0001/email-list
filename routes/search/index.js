574ce379a9c8f55c0a603e3ee265ff3839b33623
Stephen Tillman <askmrtillman@gmail.com>
2021-10-22 21:21:51 -0400

    use post method for search route; add todo notes

--- routes/search/index.js
+++ routes/search/index.js
@@ -1,7 +1,18 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return request.query;
+  fastify.post('/', async function (request, reply) {
+    if(request.query.address){
+      const { address } = request.query;
+
+      // TODO:
+      // validate email address
+      // find email in airtable
+      // : if found, return entire record
+      // : if not found, return null
+
+      return address;
+    }
+    return null;
   })
 }
