b8fc7d26aa9875981ecd227f938491f24480d220
Stephen Tillman <askmrtillman@gmail.com>
2021-10-23 03:39:45 -0400

    fix regex

--- services/airtable-client.js
+++ services/airtable-client.js
@@ -14,9 +14,9 @@ module.exports = class AirtableClient {
    if(res.ok){
      const response = await res.json();
      const addresses = response.records;
-     const rgx = new RegExp(emailAddress.trim(),'i');
+     const rgx = new RegExp(`${emailAddress}`,'i');
      return addresses.filter(address => (
-       rgx.test(address.fields["Email Address"])
+      rgx.test(address.fields["Email Address"])
      ));
    }
    return [];
